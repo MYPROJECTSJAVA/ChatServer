f9978cc6d12baaee1b99daecf12552092ef055a6
Avishkar <avishkarshrestha045@gmail.com>
2024-10-16 10:18:31 -0300

    lets roll

--- src/Server.java
+++ src/Server.java
@@ -3,12 +3,27 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Server{
     //a variable to save the output object for each client
     static ServerSocket server;
     static String inMessage="";
     static String outMessage="";
+    static private Map<Socket, PrintWriter> clientWriters = new HashMap<>();
+    public static void broadcast(Socket sender,String message){
+        clientWriters.forEach((key,val)->{
+
+            if(sender!=key) {
+                System.out.println("sent to" + key + val);
+                val.println(message);
+                val.flush(cmd);
+            }
+
+        });
+    }
     public static void main(String args[]) {
         try {
             //create a server object
@@ -21,23 +36,31 @@ public class Server{
 
             while(true) {
 
-
+                Socket clientSocket = server.accept();
 
 
                 //objects used to send and receive a particular input and output to the respective client
                 new Thread(()->{try{
-                    Socket clientSocket = server.accept();
+
+
 
                     System.out.println("ClientSocket connected");
                     PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
                     BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    clientWriters.put(clientSocket,output);
                     output.println("Welcome to the Group Chat");
-                    while ((inMessage = input.readLine()) != "stop") {
-                        System.out.println("Read message");
-                        System.out.println(inMessage);
-                        output.println("Received");
-                        System.out.println("Message sent");
+                    while ((inMessage = input.readLine()) != "stop" && inMessage!=null) {
+                        if(!Objects.equals(inMessage, "")) {
+                            System.out.println("Read message");
+                            System.out.println(inMessage);
+                            //broadcast inMessage to everyone
+                            broadcast(clientSocket, inMessage);
+
+                            System.out.println("Message sent");
+                        }
                     }
+                    clientWriters.remove(clientSocket);
+
                     output.close();
                     input.close();
                     clientSocket.close();
